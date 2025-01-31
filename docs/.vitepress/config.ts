111bdf7a0280e4e1269398e2f7e2ee0f7b228e45
Chencyyy <chengying.yu85@gmail.com>
2025-01-31 13:55:32 -0500

    底层文件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vitepress';
 
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
-  lang: 'en-US',
-  title: 'VitePress',
-  description: 'Vite & Vue powered static site generator.',
+  base: '/NOTEBOOK/',
+  title: 'NOTEBOOK',
+  description: '',
 
   themeConfig: {
     nav: [
