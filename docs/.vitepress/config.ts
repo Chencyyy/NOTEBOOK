410e1205a8a521b9595fcbeb5ca4c5bbbd6d25b1
errrr-er <luj13014@gmail.com>
2025-02-01 14:43:50 -0500

    ？

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -53,44 +53,40 @@ export default defineConfig({
     ],
 
     sidebar: [
-      {
+      { text: 'DND',
+        collapsed: true,
         items: [
-          { text: 'DND',
+          { text: 'Wiki', link: '/dnd/wiki'},
+          { text: '角色',
             collapsed: true,
             items: [
-              { text: 'Wiki', link: '/dnd/wiki'},
-              { text: '角色',
-                collapsed: true,
-                items: [
-                  { text: 'Bell', link: '/dnd/bell/index'}
-                ]
-              }
+              { text: 'Bell', link: '/dnd/bell/index'}
             ]
-          },
-          { text: '鸣潮', link: '/wuthering/wuwa_echo'},
-          { text: 'COD 剧情解说', link: '/cod/cod_story'},
-          { text: 'Sealdice',
-            collapsed: true,
-            items: [
-              { text: '海豹手册', link: 'https://docs.sealdice.com/'},
-              { text: '分离部署',
-                collapsed: true,
-                items: [
-                  { text: '指导手册', link: 'https://lagrangedev.github.io/Lagrange.Doc/Lagrange.OneBot/Config/'},
-                  { text: '使用文件', link: 'https://github.com/LagrangeDev/Lagrange.Core/releases'}
-              ]
-              },
-              { text: '其它',
-                collapsed: true,
-                items: [
-                  { text: '代挂事宜', link: '/sealdice/m522'}
-                ]
-              }
-            ]
-          },
-          { text: 'QQ 相关链接', link: '/qq/qq_helpful_links'},
-        ],
+          }
+        ]
       },
+      { text: '鸣潮', link: '/wuthering/wuwa_echo'},
+      { text: 'COD 剧情解说', link: '/cod/cod_story'},
+      { text: 'Sealdice',
+        collapsed: true,
+        items: [
+          { text: '海豹手册', link: 'https://docs.sealdice.com/'},
+          { text: '分离部署',
+            collapsed: true,
+            items: [
+              { text: '指导手册', link: 'https://lagrangedev.github.io/Lagrange.Doc/Lagrange.OneBot/Config/'},
+              { text: '使用文件', link: 'https://github.com/LagrangeDev/Lagrange.Core/releases'}
+          ]
+          },
+          { text: '其它',
+            collapsed: true,
+            items: [
+              { text: '代挂事宜', link: '/sealdice/m522'}
+            ]
+          }
+        ]
+      },
+      { text: 'QQ 相关链接', link: '/qq/qq_helpful_links'},
     ],
   },
 });
